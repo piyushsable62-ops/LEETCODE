fe44576ead3887e50e15e57c81cab5f293dd5ee0
PIYUSH SABLE <piyushsable62@gmail.com>
2026-06-23 14:08:49 +0530

    Time: 5 ms (8.55%), Space: 48.3 MB (6.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,31 +1,35 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        for(int i = 0;i<nums.length;i++){
-            for(int j = 0; j<nums.length-1;j++){
-                if(nums[j] == 0 ){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
+    public void moveZeroes(int[] arr) {
+        // for(int i = 0;i<nums.length;i++){
+        //     for(int j = 0; j<nums.length-1;j++){
+        //         if(nums[j] == 0 ){
+        //             int temp = nums[j];
+        //             nums[j] = nums[j+1];
+        //             nums[j+1] = temp;
 
-                }
-            }
-        }
-        // ArrayList<Integer> arr = new ArrayList<>();
-        // int count = 0;
-        // for(int i = 0 ; i<nums.length ;i++){
-        //     if(nums[i] != 0){
-        //         arr.add(nums[i]);
-        //     }else{
-        //         count++;
+        //         }
         //     }
         // }
-        // for(int j = 0; j<count;j++){
-        //     arr.add(0);
-        // }
-        // for(int k = 0 ; k<nums.length ;k++ ){
-        //     nums[k] = arr.get(k);
-
-        // }
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] != 0){
+                list.add(arr[i]);
+            }else{
+                count++;
+            }
+            
+        }
+        for(int i = 0 ;i<count;i++){
+            list.add(0);
+        }
+        for(int j = 0;j <arr.length;j++){
+            
+                arr[j] = list.get(j) ;
+            
+        }
+       
+       
 
         
     }
