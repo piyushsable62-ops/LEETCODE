ad2874dc5a14d3814aafc7fd984c1a4d2aecb06a
PIYUSH SABLE <piyushsable62@gmail.com>
2026-06-28 19:24:38 +0530

    Time: 2 ms (96.07%), Space: 45.4 MB (10.65%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,33 +1,79 @@
 class Solution {
+
+    // Merge Sort
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
+    }
+
     public int[] intersect(int[] nums1, int[] nums2) {
-         Arrays.sort(nums1);
-        Arrays.sort(nums2);
+
+        mergeSort(nums1, 0, nums1.length - 1);
+        mergeSort(nums2, 0, nums2.length - 1);
+
         ArrayList<Integer> list = new ArrayList<>();
-        int i = 0;
-        int j = 0 ;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i] > nums2[j]){
+
+        int i = 0, j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } 
+            else if (nums1[i] > nums2[j]) {
+                j++;
+            } 
+            else {
+                list.add(nums1[i]);
+                i++;
                 j++;
             }
-            else if(nums1[i] < nums2[j]){
-                i++;
-            }else {
-                 list.add(nums1[i]);  
-                 i++;
-                 j++;   
         }
-        }
-        int[] arr = new int[list.size()];
-        for( i = 0;i<list.size();i++){
-            arr[i] = list.get(i);
-            
 
+        int[] res = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            res[k] = list.get(k);
         }
-        
-        
-        
-    
-    return arr;
-        
+
+        return res;
     }
 }
