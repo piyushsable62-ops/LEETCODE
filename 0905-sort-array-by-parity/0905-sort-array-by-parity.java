a75174a423993232d8a218d62b5729ecbe3ab6c0
PIYUSH SABLE <piyushsable62@gmail.com>
2026-06-29 14:03:33 +0530

    Time: 1 ms (49.51%), Space: 46.8 MB (68.35%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -31,3 +31,21 @@ class Solution {
         
     }
 }
+// gpt soln
+// class Solution {
+//     public int[] sortArrayByParity(int[] nums) {
+//         int i=0;
+//         int j=nums.length-1;
+//         while(i<j){
+//             if(nums[j] % 2==0){
+//                 int temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;
+//                 i++;
+//             }else{
+//                 j--;
+//             }
+//         }
+//         return nums;
+//     }
+// }
