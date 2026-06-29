10e5274b3e0bb68d74b690819cab484220f78f74
PIYUSH SABLE <piyushsable62@gmail.com>
2026-06-29 14:01:02 +0530

    Time: 0 ms (100%), Space: 46.6 MB (84.22%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        if(nums.length == 1){
+            return nums;
+        }
+        int i = 0;
+        int j = 1;
+        while(i<nums.length && j<nums.length){
+            if(nums[i]%2 == 0){
+                i++;
+                j++;
+            }
+            else if(nums[i]%2 == 1 && nums[j]%2 == 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+            }else if(nums[i]%2==1 && nums[j]%2==1){
+                j++;
+            }
+            else{
+                i++;
+                j++;
+            }
+            
+
+        }
+        return nums;
+
+        
+    }
+}
