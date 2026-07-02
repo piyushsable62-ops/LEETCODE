772b00f905644f7301bb6f745aa82afb3abbc6a7
PIYUSH SABLE <piyushsable62@gmail.com>
2026-07-02 20:51:42 +0530

    Time: 93 ms (7.55%), Space: 45.7 MB (36.76%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+//         int[] ans = new int[nums.length];
+//         for(int i = 0 ;i < nums.length;i++){
+//             int count = 0;
+//             for(int j = 0;j<nums.length;j++){
+//                 if(nums[j] < nums[i] && j!=i){
+//                     count++;
+//                 }
+//             }
+//             ans[i]=count;
+//         }
+//         return ans;
+        
+//     }
+// }
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] ans = new int[nums.length];
-        for(int i = 0 ;i < nums.length;i++){
-            int count = 0;
-            for(int j = 0;j<nums.length;j++){
-                if(nums[j] < nums[i] && j!=i){
-                    count++;
-                }
-            }
-            ans[i]=count;
+        int[] freq = new int[101];
+
+        for (int num : nums) {
+            freq[num]++;
         }
+
+        for (int i = 1; i < 101; i++) {
+            freq[i] += freq[i - 1];
+        }
+
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                ans[i] = 0;
+            else
+                ans[i] = freq[nums[i] - 1];
+        }
+
         return ans;
-        
     }
 }
