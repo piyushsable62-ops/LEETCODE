ac5ca7e6d830488725bf202c47f7023cf48b3d67
PIYUSH SABLE <piyushsable62@gmail.com>
2026-07-02 21:03:44 +0530

    Time: 0 ms (100%), Space: 45.7 MB (39.6%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double average(int[] salary) {
+        int sum = 0;
+        int count = 0;
+        int max= Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i<salary.length;i++){
+            if(salary[i] > max){
+                max = salary[i];
+            }
+        }for(int i = 0;i<salary.length;i++){
+            if(salary[i] < min){
+                min = salary[i];
+            }
+        }
+        for(int i = 0;i<salary.length;i++){
+         if(salary[i]!= min && salary[i] != max){
+         sum+=salary[i];
+         }
+        }
+        return (double)sum/(salary.length-2);
+        
+    }
+}
