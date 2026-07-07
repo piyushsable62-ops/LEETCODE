e6c375ad0841c9f966edb9791d1e603a9e5d26ec
PIYUSH SABLE <piyushsable62@gmail.com>
2026-07-07 21:38:36 +0530

    Time: 0 ms (100%), Space: 44 MB (20.11%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,46 +1,66 @@
 class Solution {
     public int findMin(int[] nums) {
-        int x = binary(nums);
+    //     int x = binary(nums);
 
 
-        if(nums.length == 0 || x == 0){
-            return nums[0];
-        }
-        swap(nums,0,x-1);
-        swap(nums,x,nums.length-1);
-        swap(nums,0,nums.length-1);
-        return nums[0];
-    }
-    public int binary(int[] arr){
-        int i = 0;
-        int j = arr.length-1;
-        int x = 0;
-        while(i<=j){
-            int mid = (j-i)/2+i;
+    //     if(nums.length == 0 || x == 0){
+    //         return nums[0];
+    //     }
+    //     swap(nums,0,x-1);
+    //     swap(nums,x,nums.length-1);
+    //     swap(nums,0,nums.length-1);
+    //     return nums[0];
+    // }
+    // public int binary(int[] arr){
+    //     int i = 0;
+    //     int j = arr.length-1;
+    //     int x = 0;
+    //     while(i<=j){
+    //         int mid = (j-i)/2+i;
            
-            if( mid>0 && arr[mid] < arr[mid-1]){
-                x = mid;
-                return mid;
+    //         if( mid>0 && arr[mid] < arr[mid-1]){
+    //             x = mid;
+    //             return mid;
                
-            }
+    //         }
             
-            else if(arr[mid] >= arr[0]){
-                i = mid+1;
-            }else {
-                j = mid -1;
-            }
-        }
-        return x;
+    //         else if(arr[mid] >= arr[0]){
+    //             i = mid+1;
+    //         }else {
+    //             j = mid -1;
+    //         }
+    //     }
+    //     return x;
         
        
+    // }
+    // public void swap(int[] arr,int i ,int j){
+    //   while(i<=j){
+    //     int temp = arr[i];
+    //     arr[i] = arr[j];
+    //     arr[j] = temp;
+    //     i++;
+    //     j--;
+    //   }
+   if(nums.length == 1){
+    return nums[0];
+
+   }
+     
+   int i = 0;
+   int j = nums.length-1;
+   while(i<=j){
+    int mid = (j-i)/2+i;
+    if(mid>0 && nums[mid-1] > nums[mid]){
+        return nums[mid];
+    }else if(nums[mid] >= nums[0]){
+        i = mid+1;
+    }else{
+        j = mid-1;
     }
-    public void swap(int[] arr,int i ,int j){
-      while(i<=j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        i++;
-        j--;
-      }
+
+   }
+   return nums[0];
+
     }
 }
