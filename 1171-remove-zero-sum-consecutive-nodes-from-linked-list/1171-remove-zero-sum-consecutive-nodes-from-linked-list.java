34cd6339fff84ee2a4f7f29e71c506401d503c91
PIYUSH SABLE <piyushsable62@gmail.com>
2026-07-26 14:58:47 +0530

    Time: 5 ms (8.64%), Space: 45.1 MB (90.85%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -0,0 +1,62 @@
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+
+        ListNode temp = head;
+
+        int count = 0;
+        while (temp != null) {
+            temp = temp.next;
+            count++;
+        }
+
+        temp = head;
+
+        int[] ans = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            ans[i] = temp.val;
+            temp = temp.next;
+        }
+
+        for (int i = 0; i < ans.length; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < ans.length; j++) {
+
+                sum += ans[j];
+
+                if (sum == 0) {
+
+                    int size = ans.length - (j - i + 1);
+                    int[] newArr = new int[size];
+
+                    int k = 0;
+
+                    for (int x = 0; x < ans.length; x++) {
+
+                        if (x >= i && x <= j) {
+                            continue;
+                        }
+
+                        newArr[k++] = ans[x];
+                    }
+
+                    ans = newArr;
+                    i = -1;
+                    break;
+                }
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode t = dummy;
+
+        for (int i = 0; i < ans.length; i++) {
+            t.next = new ListNode(ans[i]);
+            t = t.next;
+        }
+
+        return dummy.next;
+    }
+}
