733ee50fa982e92dcf6ac3e5a586b268fd674ef8
PIYUSH SABLE <piyushsable62@gmail.com>
2026-08-30 21:43:57 +0530

    Time: 0 ms (100%), Space: 42.7 MB (95.34%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+      if(root == null){
+        return root;
+      }
+      bstToGst(root.right);
+      sum+=root.val;
+      root.val = sum;
+      bstToGst(root.left);
+       return root; 
+    }
+}
