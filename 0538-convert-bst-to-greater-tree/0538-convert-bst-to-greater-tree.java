301c7634bf7dd3404eaf923e79e9feed0f3d0867
PIYUSH SABLE <piyushsable62@gmail.com>
2026-09-05 20:29:23 +0530

    Time: 0 ms (100%), Space: 47.5 MB (41.83%) - LeetHub

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    int sum = 0 ;
+    public TreeNode convertBST(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        convertBST(root.right);
+        sum =  sum+root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+        
+    }
+}
